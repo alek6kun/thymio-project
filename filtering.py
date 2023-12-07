485adfae6adefb56cbdb3ce1930e75146b21fece
sergeelasmar <126913648+sergeelasmar@users.noreply.github.com>
2023-12-07 03:12:09 +0100

    filtering finished, added sleeps to main, slightly enlarged area of nodes in vision to 0.13

--- filtering.py
+++ filtering.py
@@ -1,6 +1,7 @@
 from filterpy.kalman import KalmanFilter
 import numpy as np
 import cv2
+import math
 
 
 ## General parameters
@@ -23,39 +24,43 @@ f.H = np.eye(3)
 # Initial covariance matrix
 f.P = np.eye(3) * 100
 # Measurement noise
-camera_variances = [1.13554018e-01, 1.93571267e-01, 2.02748876e-05]
+camera_variances = [2.13554018e-01, 2.93571267e-01, 6.02748876e-05]
 f.R = np.diag(camera_variances)
 # Process noise
-process_variances = [3.8751605996417765e-010, 3.8751605996417765e-010, 2.9656863710880975e-05]
+process_variances = [3.8751605996417765e-01, 3.8751605996417765e-01, 2.9656863710880975e-03]
 f.Q = np.diag(process_variances)
 
 
 def run_filter(speed_right, speed_left, prev_angle, vis):
     global f
-    
+
     camera_scale = vis.scale # [camera_coordinates/m]
     # Converting the motors to rad/s
     speed_right = speed_right / motor_scale
     speed_left = speed_left / motor_scale
+    
     # Defining control input and control transition matrix
     u = np.array([[speed_right],
                   [speed_left]])
     B = np.array([[np.cos(prev_angle)*(dt/2), np.cos(prev_angle)*(dt/2)],
                     [np.sin(prev_angle)*(dt/2), np.sin(prev_angle)*(dt/2)],
                     [(-dt/d), (dt/d)]]) * R
+    
     # Getting camera measurements and conveting to [m]
     measurement = np.array([vis.robot.x/camera_scale,vis.robot.y/camera_scale,-vis.robot.angle])
     
+    # Predict step of kalman filter with control input
     f.predict(u = u, B = B)
     # Only update if we have new camera measurements
     if vis.found_robot:
         f.update(measurement)
-    else:
-        print("predicted states:", (f.x[0,0],f.x[1,0],f.x[2,0]))
 
     # Defining the estimate in camera coordinates
-    print("Det P", np.linalg.det(f.P))
     estimate = np.array([f.x[0,0] * camera_scale, f.x[1,0] * camera_scale, f.x[2,0]])
-    cv2.circle(vis.copy,(int(estimate[0]), int(estimate[1])),int(np.linalg.det(f.P)*(10**8)/1.2),(255,0,0),3)
+
+    # Plotting an ellipse to show evolution of uncertainty along x and y
+    cv2.ellipse(vis.copy, (int(estimate[0]), int(estimate[1])), (int(200 * f.P[0,0]), int(200 * f.P[1,1])), math.degrees(f.x[2,0]),0,360,(255,0,0),3)
+    # Plotting a line to show the angle estimate
     cv2.line(vis.copy, (int(estimate[0]), int(estimate[1])), (int(estimate[0] + 100*np.cos(estimate[2])), int(estimate[1] + 100*np.sin(estimate[2]))), (255,0,0), 3)
+    
     return estimate # Return the kalman filtered state
